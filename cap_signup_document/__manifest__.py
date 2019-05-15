c06dd690fa26dea7c2a2b913694b3381456ae86f
NathanQj <nathan.qjm@gmail.com>
2019-05-15 14:43:06 -0400

    Add files via upload

---
+++ cap_signup_document/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'cap_signup_document',
+    'author': 'nathanqj',
+    'version': '1.0',
+    'depends': ['base','contact','studio_customization'],
+    'description': """
+    """,
+    "qweb": [],
+    'data': [
+            ],
+    'installable': True
+}
---
+++ cap_signup_document/models/new_contact.py
@@ -0,0 +1,12 @@
+from odoo import api, fields, models, _
+from odoo.exceptions import UserError
+
+# override do_signup method to allow referred_by_id to be set from sign-up form
+class AuthSignupHome(AuthSignupHome):
+    def do_signup(self, qcontext):
+        """ Shared helper that creates a res.partner out of a token """
+        values = dict((key, qcontext.get(key)) for key in ('login', 'name', 'password', 'x_studio_additional_info'))
+        assert any([k for k in values.values()]), "The form was not properly filled in."
+        assert values.get('password') == qcontext.get('confirm_password'), "Passwords do not match; please retype them."
+        self._signup_with_values(qcontext.get('token'), values)
+        request.cr.commit()
